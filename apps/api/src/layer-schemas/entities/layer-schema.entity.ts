12eabd099326291c4867274bbd245a57cf29d655
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2025-04-25 20:48:35 -0300

    fix: move common to a package

--- apps/api/src/layer-schemas/entities/layer-schema.entity.ts
+++ apps/api/src/layer-schemas/entities/layer-schema.entity.ts
@@ -8,7 +8,7 @@ import {
   OneToMany,
   PrimaryColumn,
 } from 'typeorm';
-import { ClickActionEnum } from './../../../../common/enums/click-action.enum';
+import { ClickActionEnum } from '@open-urbis/map-shared';
 import { LayerSchemaColors } from './layer-schema-color.entity';
 
 export interface IClickAction {
--- apps/api/src/search-config/entities/search-config.entity.ts
+++ apps/api/src/search-config/entities/search-config.entity.ts
@@ -1,7 +1,7 @@
 import { LayerSchema } from 'layer-schemas/entities/layer-schema.entity';
 import { SearchConfigMethodEnum } from 'search-config/enums/search-config.enum';
 import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
-import { ClickActionEnum } from './../../../../common/enums/click-action.enum';
+import { ClickActionEnum } from '@open-urbis/map-shared';
 
 export interface IClickAction {
   action: ClickActionEnum;

74941db1e05ffb20a68b3c64617b2b0efe3db0a4
Douglas Gabriel Cardoso <32394842+douglasgc@users.noreply.github.com>
2025-05-13 19:57:38 -0300

    Feature/reponsive (#20)
    * lock: file lock version
    
    * feat: reponsive map navigation

--- apps/web/src/hooks/useFetchIntersectingPolygons.ts
+++ apps/web/src/hooks/useFetchIntersectingPolygons.ts
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { getIntersections } from "../integrations/map-integration";
 import { Polygon, ResponseData } from "../types/fetch-map-intersections-type";
+import { useNavigationContext } from "./useNavigationContext";
 
 export const useFetchIntersectingPolygons = () => {
   const [data, setData] = useState<ResponseData | null>(null);
 
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const {toggleDrawer} = useNavigationContext();
 
   const reset = () => {
     setLoading(false);
@@ -20,6 +22,7 @@ export const useFetchIntersectingPolygons = () => {
     try {
       const response = await getIntersections(polygon);
       setData(response);
+      toggleDrawer();
     } catch (err) {
       console.error(err);
       setError("Error fetching intersecting polygons");

b6ef163ead6f37fab90e87b686a63a1003871aae
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2025-06-24 14:33:09 -0300

    fix: missing perimetro

--- apps/api/src/geospatial-intersection/geospatial-intersection.service.ts
+++ apps/api/src/geospatial-intersection/geospatial-intersection.service.ts
@@ -248,7 +248,7 @@ export class GeospatialIntersectionService {
       // Structure the response
       const allFields = {
         cd_sql: sqlc,
-        lote: lotFeature,
+        perimetro: lotFeature,
         geom_lote: lotFeature.geometry,
         geom_zoneamento_2016: intersections.features
           .filter(f => f.properties.layer === 'slui:zoneamento')

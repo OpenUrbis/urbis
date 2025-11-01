ddda3e50c0d29ca04ef30edc9c9bc6dde75d3948
gvrDev <guilhermevrodrigues123@gmail.com>
2025-11-01 09:57:18 -0300

    feat: adds entities for shared and app-wise whitelabel theming

---
+++ apps/api/src/organization/entities/organization-application-whitelabel.entity.ts
@@ -0,0 +1,45 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApplicationName } from '../enums/application-name.enum';
+import { ApplicationTheme } from '../enums/application-theme.enum';
+import { Organization } from './organization.entity';
+
+@Entity('organization_application_whitelabels')
+export class OrganizationApplicationWhitelabel extends BaseEntity {
+  @PrimaryColumn('uuid')
+  organizationId: string;
+
+  @PrimaryColumn({
+    type: 'enum',
+    enum: ApplicationName,
+  })
+  application: ApplicationName;
+
+  @Column({
+    type: 'enum',
+    enum: ApplicationTheme,
+  })
+  theme?: ApplicationTheme | null;
+
+  @OneToOne(() => Organization, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'organizationId' })
+  organization: Organization;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}

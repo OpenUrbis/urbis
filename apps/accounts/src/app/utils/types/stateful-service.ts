7d50dae3f34d9c9ba56c0b3503999e1eb31923ef
gvrDev <guilhermevrodrigues123@gmail.com>
2025-11-03 16:53:41 -0300

    feat: adds a public readonly accessor to the state of StatefulService

--- apps/accounts/src/app/utils/types/stateful-service.ts
+++ apps/accounts/src/app/utils/types/stateful-service.ts
@@ -1,14 +1,16 @@
-import { signal, WritableSignal } from '@angular/core';
+import { signal, WritableSignal, Signal } from '@angular/core';
 
 export abstract class StatefulService<T> {
-  protected state: WritableSignal<T>;
+  protected _state: WritableSignal<T>;
+  public state: Signal<T>;
   protected setStateProperty(property: keyof T, value: T[keyof T]) {
-    this.state.set({
-      ...this.state(),
+    this._state.set({
+      ...this._state(),
       [property]: value,
     });
   }
   constructor(initialState: T) {
-    this.state = signal<T>(initialState);
+    this._state = signal<T>(initialState);
+    this.state = this._state.asReadonly();
   }
 }

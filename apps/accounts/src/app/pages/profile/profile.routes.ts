db4daeed538caaa17221b532a151820898bd77de
gvrDev <guilhermevrodrigues123@gmail.com>
2025-11-05 21:21:02 -0300

    feat: initial iteration of edit profile page

--- apps/accounts/src/app/pages/profile/profile.routes.ts
+++ apps/accounts/src/app/pages/profile/profile.routes.ts
@@ -5,4 +5,9 @@ export const profileRoutes: Routes = [
     path: '',
     loadComponent: () => import('./profile').then((c) => c.Profile),
   },
+  {
+    path: 'edit',
+    loadComponent: () =>
+      import('./pages/profile-edit/profile-edit').then((c) => c.ProfileEdit),
+  },
 ];

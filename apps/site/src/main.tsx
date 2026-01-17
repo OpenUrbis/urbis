b4b9864dc76e7f26ff745bf8a0b04ef6b8eb24b9
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2026-01-17 12:25:18 -0300

    cicd: update site error build

--- apps/site/src/main.tsx
+++ apps/site/src/main.tsx
@@ -2,8 +2,7 @@ import { ViteReactSSG } from 'vite-react-ssg'
 import routes from './routes'
 import './globals.css'
 
-export const createApp = ViteReactSSG(
-  // @ts-expect-error - Vite types mismatch
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  { routes, base: import.meta.env.BASE_URL }
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+export const createRoot = ViteReactSSG(
+  { routes, basename: import.meta.env.BASE_URL }
 )

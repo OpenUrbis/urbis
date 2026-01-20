729751342593cd4a35de586e99c68bae50cbfa32
Junior Anzolin <junioranzolin58@gmail.com>
2026-01-20 04:55:51 -0300

    fix: support module test

--- apps/api/src/support/support.service.spec.ts
+++ apps/api/src/support/support.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { SupportTicket } from './entities/support-ticket.entity';
 import { SupportService } from './support.service';
 
 describe('SupportService', () => {
   let service: SupportService;
 
+  const mockRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SupportService],
+      providers: [
+        SupportService,
+        {
+          provide: getRepositoryToken(SupportTicket),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<SupportService>(SupportService);

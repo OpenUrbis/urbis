b13c929293c4ac7f5574ef189e8f00eaf9443baf
Junior Anzolin <junioranzolin58@gmail.com>
2026-01-28 13:58:40 -0300

    feat: multi upload

--- apps/api/src/files/files.controller.ts
+++ apps/api/src/files/files.controller.ts
@@ -74,4 +74,26 @@ export class FilesController {
   async getDownloadUrl(@Query() query: DownloadUrlDto) {
     return this.filesService.getDownloadUrl(query.key);
   }
+
+  @Get('public/download-url')
+  @Recaptcha({
+    response: (req) => req.headers.recaptcha,
+    action: 'download_url',
+    score: 0.5,
+  })
+  @ApiOperation({ summary: 'Generate S3 download URL with Recaptcha' })
+  @ApiQuery({
+    name: 'key',
+    type: String,
+    required: true,
+    example: 'uploads/file.jpg',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'URL generated',
+    schema: { example: { url: 'https://s3.amazonaws.com/...' } },
+  })
+  async getPublicDownloadUrl(@Query() query: DownloadUrlDto) {
+    return this.filesService.getDownloadUrl(query.key);
+  }
 }

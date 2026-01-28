50fc91ef112a1ac9407ea80d1a5393ee7282dcde
Junior Anzolin <junioranzolin58@gmail.com>
2026-01-28 21:34:24 +0800

    fix: merge conflict

--- apps/web/src/components/LeftNav/index.tsx
+++ apps/web/src/components/LeftNav/index.tsx
@@ -15,13 +15,13 @@ export const LeftNav = () => {
           "relative h-full transition-all duration-300 flex flex-col pt-16 w-[420px] pointer-events-none",
           drawerOpen.value
             ? "bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-r"
-            : "bg-transparent backdrop-blur-none border-transparent"
+            : "bg-transparent backdrop-blur-none border-transparent",
         )}
       >
         <div
           className={cn(
             "flex-1 overflow-y-auto p-3 space-y-4 w-[420px] overflow-x-hidden",
-            drawerOpen.value ? "pointer-events-auto" : "pointer-events-none"
+            drawerOpen.value ? "pointer-events-auto" : "pointer-events-none",
           )}
         >
           <div className="pointer-events-auto">
@@ -32,28 +32,12 @@ export const LeftNav = () => {
               "transition-all duration-500 ease-in-out origin-top",
               drawerOpen.value
                 ? "opacity-100 translate-y-0 h-auto"
-                : "opacity-0 -translate-y-4 h-0 overflow-hidden pointer-events-none"
+                : "opacity-0 -translate-y-4 h-0 overflow-hidden pointer-events-none",
             )}
           >
             {currentPage.value}
           </div>
         </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <div
-          className={cn(
-            "w-[420px] transition-all duration-300",
-            drawerOpen.value
-              ? "border-t bg-background opacity-100 pointer-events-auto"
-              : "border-transparent bg-transparent opacity-0 h-0 overflow-hidden pointer-events-none"
-          )}
-        >
-          <UrbisFooter />
-        </div>
-=======
->>>>>>> e4cd745 (adjusting footer on web)
-=======
->>>>>>> 160c23a77887189628a4c976f2e6af7024ad6327
       </div>
     );
   }

3e9345c9dcf5e9243534a081b7a00c8dfe15e0b3
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2026-01-29 18:55:47 -0300

    fix: build urbis map

--- apps/web/src/components/LeftNav/index.tsx
+++ apps/web/src/components/LeftNav/index.tsx
@@ -4,7 +4,6 @@ import {
   Drawer,
   DrawerContent,
   DrawerTitle,
-  UrbisFooter,
 } from "@open-urbis/map-ui";
 import { useNavigationContext } from "../../hooks/useNavigationContext";
 import { Search } from "../Search";

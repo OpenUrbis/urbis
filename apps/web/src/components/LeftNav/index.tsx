0bd62e9b2e66e37fdbe6fe022f210265bf0dea36
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2026-01-29 18:44:00 -0300

    fix: map and infra small fixes

--- apps/web/src/components/LeftNav/index.tsx
+++ apps/web/src/components/LeftNav/index.tsx
@@ -43,16 +43,6 @@ export const LeftNav = () => {
             {currentPage.value}
           </div>
         </div>
-        <div
-          className={cn(
-            "w-[420px] transition-all duration-300",
-            drawerOpen.value
-              ? "border-t bg-background opacity-100 pointer-events-auto"
-              : "border-transparent bg-transparent opacity-0 h-0 overflow-hidden pointer-events-none",
-          )}
-        >
-          <UrbisFooter />
-        </div>
       </div>
     );
   }
@@ -70,7 +60,6 @@ export const LeftNav = () => {
         <div className="flex-1 overflow-y-auto p-4 space-y-4">
           <Search />
           {currentPage.value}
-          <UrbisFooter />
         </div>
       </DrawerContent>
     </Drawer>

bb2e88d01bc2a8332df7d56d7ff2dcd7290b1273
Junior Anzolin <32394862+junior-anzolin@users.noreply.github.com>
2026-02-07 12:36:55 -0300

    fix: error on polygon map

--- apps/web/src/utils/createFn.ts
+++ apps/web/src/utils/createFn.ts
@@ -1,11 +1,14 @@
 import { calculateCenterId } from "../hooks/useMapContext";
 
-export const createFn = (strFn: string) => {
+export const createFn = (strFn: string, skipError = true) => {
   try {
-    const fn = new Function("utils", `return ${strFn}`);
+    const fn = new Function(
+      "utils",
+      `return ${strFn.replace(/\n\s*/g, "").replace(/\u003E/g, ">")}`
+    );
     return fn({ calculateCenterId });
   } catch (error) {
     console.error("Error creating function from string:", strFn, error);
-    return () => {};
+    return skipError ? () => {} : undefined;
   }
 };

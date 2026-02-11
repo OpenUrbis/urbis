e1177e0dce3d7a8fa3fff1fffd209c146104a792
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2026-02-10 22:57:46 -0300

    fix: throtling fix logs

--- apps/api/src/common/middleware/throttler-headers.middleware.ts
+++ apps/api/src/common/middleware/throttler-headers.middleware.ts
@@ -1,14 +1,12 @@
-import {
-  Injectable,
-  NestMiddleware,
-} from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class ThrottlerHeadersMiddleware implements NestMiddleware {
   constructor() {}
 
-  async use(req: Request, res: Response, next: NextFunction) {
+  async use(_req: Request, _res: Response, next: NextFunction) {
+    await Promise.resolve();
     next();
   }
 }
